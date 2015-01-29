91ee5384010d7a2515c063f84672b114da90f90d
Razvan Pascalau <mail@razvan-pascalau.com>
2015-01-29 21:58:42 +0200

    initial commit

---
+++ EnumBitMask/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EnumBitMask
+//
+//  Created by Razvan Pascalau on 29/01/15.
+//  Copyright (c) 2015 Razvan Pascalau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
