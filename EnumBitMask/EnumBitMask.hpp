58aacaefcb4813f5ecaf13b6cd59643f9d93a6d5
Razvan Pascalau <mail@razvan-pascalau.com>
2016-01-08 23:02:00 +0200

    added set to class

--- EnumBitMask/EnumBitMask.hpp
+++ EnumBitMask/EnumBitMask.hpp
@@ -7,12 +7,65 @@
 //
 
 #pragma once
+#include <algorithm>
+#include <initializer_list>
 #include <type_traits>
 
 template <typename EnumType>
-class EnumBitMask
-{
+class EnumBitMask {
+    static_assert(std::is_enum<EnumType>::value, "This type works only with enums as flags");
+
+public:
+    EnumBitMask();
+    explicit EnumBitMask(const std::initializer_list<EnumType>& initFlags);
+
+    bool isSet(EnumType enumFlag) const;
+    template <typename... Params>
+    bool areSet(EnumType enumFlag, Params... nextEnumFlags) const;
+
+    void set(EnumType flag);
+
 private:
     using EnumUnderlyingType = typename std::underlying_type<EnumType>::type;
+
+    bool areSet(EnumType checkedEnum) const; // base version to break recursion
+
+    EnumUnderlyingType mBits = 0;
 };
 
+template <typename EnumType>
+EnumBitMask<EnumType>::EnumBitMask()
+{
+}
+
+template <typename EnumType>
+EnumBitMask<EnumType>::EnumBitMask(const std::initializer_list<EnumType>& initFlags)
+{
+    std::for_each(std::begin(initFlags), std::end(initFlags), [this, &initFlags](EnumType enumFlag) { set(enumFlag); });
+}
+
+template <typename EnumType>
+void EnumBitMask<EnumType>::set(EnumType flag)
+{
+    mBits |= static_cast<EnumUnderlyingType>(flag);
+}
+
+template <typename EnumType>
+bool EnumBitMask<EnumType>::isSet(EnumType checkedEnum) const
+{
+    auto result = (mBits & static_cast<EnumUnderlyingType>(checkedEnum)) == static_cast<EnumUnderlyingType>(checkedEnum);
+    return result;
+}
+
+template <typename EnumType>
+bool EnumBitMask<EnumType>::areSet(EnumType checkedEnum) const
+{
+    return isSet(checkedEnum);
+}
+
+template <typename EnumType>
+template <typename... Params>
+bool EnumBitMask<EnumType>::areSet(EnumType checkedEnum, Params... nextEnums) const
+{
+    return isSet(checkedEnum) && areSet(nextEnums...);
+}
--- EnumBitMask/main.cpp
+++ EnumBitMask/main.cpp
@@ -6,10 +6,22 @@
 //  Copyright (c) 2015 Razvan Pascalau. All rights reserved.
 //
 
+#include "EnumBitMask.hpp"
 #include <iostream>
+enum class TestEnum {
+    firstVal,
+    secondVal
+};
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[])
+{
     // insert code here...
+    EnumBitMask<TestEnum> myEnum{ TestEnum::firstVal };
+    auto result = myEnum.isSet(TestEnum::firstVal);
+    auto secondResult = myEnum.isSet(TestEnum::secondVal);
+    auto val = TestEnum::firstVal;
+    auto val2 = TestEnum::secondVal;
+
     std::cout << "Hello, World!\n";
     return 0;
 }
